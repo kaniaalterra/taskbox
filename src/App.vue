c2b251cf62f991a5183843539388aeead29afeed
Kania Nur Sawitri <kania@alterra.id>
2019-08-11 20:52:26 +0700

    taskbox UI

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <inbox-screen/>
+  </div>
+</template>
+
+<script>
+import store from "./store";
+import InboxScreen from "./components/InboxScreen.vue";
+import "../src/index.css";
+
+export default {
+  name: "app",
+  store,
+  components: {
+    InboxScreen
+  }
+};
+</script>
